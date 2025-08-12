e0400c92484e4b9364a305d47140e57369c2aaa5
pmendesl <pedro.gabrie2909@gmail.com>
2025-08-12 08:41:27 -0300

    Update soma.c

--- soma.c
+++ soma.c
@@ -1,4 +1,17 @@
+#include <stdio.h>
+// Função recursiva para somar de n até 1
 int soma(int n) {
-  if (n == 0) return 0;
-  return n + soma(n - 1);
+    if (n == 0) return 0; // Caso base
+    return n + soma(n - 1); // Passo recursivo
+}
+int main() {
+    int numero;
+    printf("Digite um numero inteiro positivo: ");
+    scanf("%d", &numero);
+    if (numero < 0) {
+        printf("Digite apenas numeros positivos.\n");
+    } else {
+        printf("Soma de 1 ate %d = %d\n", numero, soma(numero));
+    }
+    return 0;
 }
