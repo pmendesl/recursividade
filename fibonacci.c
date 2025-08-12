3d92e92ca04c8c50bf239f494de208ed2af9741a
pmendesl <pedro.gabrie2909@gmail.com>
2025-08-12 08:40:34 -0300

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
+// Função recursiva para calcular o n-ésimo termo da Fibonacci
 int fibonacci(int n) {
-  if (n == 0) return 0;
-  if (n == 1) return 1;
-  return fibonacci(n - 1) + fibonacci(n - 2);
+    if (n == 0) return 0; // Caso base 1
+    if (n == 1) return 1; // Caso base 2
+    return fibonacci(n - 1) + fibonacci(n - 2); // Passo recursivo
 }
 int main() {
-  int i;
-  printf("Sequência de Fibonacci (10 primeiros): \n");
-  for (i = 0; i < 10; i++) {
-    printf("%d ", fibonacci(i));
-  }
-  printf("\n");
-return 0;
+    int termos;
+    printf("Digite a quantidade de termos da sequencia de Fibonacci: ");
+    scanf("%d", &termos);
+    if (termos <= 0) {
+        printf("Digite um valor positivo.\n");
+    } else {
+        printf("Sequencia de Fibonacci (%d termos):\n", termos);
+        for (int i = 0; i < termos; i++) {
+            printf("%d ", fibonacci(i));
+        }
+        printf("\n");
+    }
+    return 0;
 }
